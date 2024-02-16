82bf7942bf16ee3026789b749a669622b832c26e
jogi takechi <jtakec@hotmail.com>
2024-02-15 22:49:09 -0300

    first commit

---
+++ blazorade.msal/2.2.0/readme.md
@@ -0,0 +1,46 @@
+﻿# Blazorade MSAL
+
+Provides easy to use authentication and token acquisition for Blazor applications with the help of Microsoft Authentication Library. Supports both Blazor Server and Blazor WebAssembly applications.
+
+## Getting Started
+
+After you have installed the package to your application, refer to the [Getting Started](https://github.com/Blazorade/Blazorade-MSAL/wiki/Getting-Started) section on the package wiki for information on how to easily get started with Blazorade MSAL.
+
+## Highlights
+
+Blazorade MSAL facilitates authentication and authorization for instance with the following services.
+
+- `BlazoradeMsalService` - A service class that handles all communication with the MSAL JavaScript library for you. You don't have to write a single line of JavaScript code in your application.
+- `BlazoradeRequestFactory` - A factory service that creates [`HttpRequestMessage`](https://docs.microsoft.com/dotnet/api/system.net.http.httprequestmessage) instances. These request messages are configured with an access token provided by `BlazoradeMsalService` which enables you to easily call into APIs such as [Microsoft Graph](https://docs.microsoft.com/graph/api/overview).
+
+These services are registered in your application's service collection with the `AddBlazoradeMsal` method as described in the [Getting Started](https://github.com/Blazorade/Blazorade-MSAL/wiki/Getting-Started#configure-blazorade-msal-for-your-application) section on the Blazoarde MSAL wiki.
+
+## Sample Applications
+
+The Github repository for Blazorade MSAL contains several sample applications that demonstrate how you can leverage Blazorade MSAL in your own application.
+
+- [**GraphClient**](https://github.com/Blazorade/Blazorade-MSAL/tree/main/GraphClient) - A WebAssembly application that demonstrates how to send HTTP requests to Microsoft Graph with the help of Blazorade MSAL. Can be applied to any other REST API that requires access tokens.
+- [**BlazorServerSample**](https://github.com/Blazorade/Blazorade-MSAL/tree/main/BlazorServerSample) - A Blazor Server application that demonstrates how you can make use of the on-demand token acquisition provided by Blazorade MSAL.
+- [**BlazorWasmSample**](https://github.com/Blazorade/Blazorade-MSAL/tree/main/BlazorWasmSample) - The same as BlazorServerSample but implemented as a Blazor WebAssembly application. Shares much of the features with the Server sample through the [SharedComponentsSample](https://github.com/Blazorade/Blazorade-MSAL/tree/main/SharedComponentsSample) component library.
+
+## Version Highlights
+
+This section lists the main improvements in each published version.
+
+### v2.2.0
+
+Added support for Azure AD B2C as Identity Provider.
+
+- [Azure AD B2C Support](https://github.com/Blazorade/Blazorade-MSAL/pull/26)
+- [Getting Started](https://github.com/Blazorade/Blazorade-MSAL/wiki/Getting-Started): Added documentation to the Getting Started section on how to configure your application for Azure AD B2C.
+
+### v2.1.0
+
+This version includes the following pull requests.
+
+- [#24 Changed methods on `BlazoradeMsalService` to virtual](https://github.com/Blazorade/Blazorade-MSAL/pull/24)
+- [#25 Added HasTokenAsync method](https://github.com/Blazorade/Blazorade-MSAL/pull/25)
+
+## Further Reading
+
+To learn more, read these [Blazorade MSAL articles](https://mikaberglund.com/tag/blazorade-msal/) on Mika Berglund's blog.
---
+++ blazorade.msal/2.2.0/staticwebassets/js/blazoradeMsal.js
@@ -0,0 +1,247 @@
+﻿
+export function acquireTokenSilent(args) {
+    console.debug("acquireTokenSilent", args);
+
+    let msalClient = createMsalClient(args);
+    console.debug("acquireTokenSilent", "msalClient", msalClient);
+
+    let account = msalClient.getAccountByUsername(args.data.loginHint);
+
+    if (!account && args.data.fallbackToDefaultLoginHint) {
+        console.debug("acquireTokenSilent", "Provided login hint did not find an account. Falling back to previously stored default login hint.");
+        account = getDefaultAccount(args, msalClient);
+    }
+
+    if (!account) {
+        // If we still don't have an account, then we need to log a warning.
+        console.warn("acquireTokenSilent", "loginHint did not find an account. Tokens can most likely not be acquired without a found account.");
+    }
+
+    console.debug("acquireTokenSilent", "account", account);
+
+    let request = {
+        account: account,
+        scopes: args.data.scopes,
+        authority: args.data.msalConfig.auth.authority
+    };
+    console.debug("acquireTokenSilent", "request", request);
+
+    try {
+        msalClient.acquireTokenSilent(request)
+            .then(result => {
+                console.debug("acquireTokenSilent", "success", result);
+                setDefaultLoginHint(args, result);
+                invokeCallback(args.successCallback, result);
+            }).catch(err => {
+                console.warn("acquireTokenSilent", "failure", err);
+                invokeCallback(args.failureCallback, err);
+            });
+    }
+    catch (err) {
+        console.error("acquireTokenSilent", "Failure calling MSAL client", err);
+        invokeCallback(args.failureCallback, err);
+    }
+}
+
+export function acquireTokenPopup(args) {
+    console.debug("acquireTokenPopup", args);
+
+    let msalClient = createMsalClient(args);
+    console.debug("acquireTokenPopup", "msalClient", msalClient);
+
+    let request = {
+        scopes: args.data.scopes,
+        authority: args.data.msalConfig.auth.authority,
+        loginHint: args.data.loginHint
+    };
+    if (args.data.prompt) request["prompt"] = args.data.prompt;
+    console.debug("acquireTokenPopup", "request", request);
+
+    try {
+        msalClient.acquireTokenPopup(request)
+            .then(result => {
+                console.debug("acquireTokenPopup", "success", result);
+                setDefaultLoginHint(args, result);
+                invokeCallback(args.successCallback, result);
+            })
+            .catch(err => {
+                console.error("acquireTokenPopup", "failure", err);
+                invokeCallback(args.failureCallback, err);
+            });
+    }
+    catch (err) {
+        console.error("acquireTokenPopup", "Failure calling MSAL client", err);
+        invokeCallback(args.failureCallback, err);
+    }
+}
+
+export function acquireTokenRedirect(args) {
+    console.debug("acquireTokenRedirect", args);
+
+    let msalClient = createMsalClient(args);
+    console.debug("acquireTokenRedirect", "msalClient", msalClient);
+
+    let request = {
+        scopes: args.data.scopes,
+        authority: args.data.msalConfig.auth.authority,
+        loginHint: args.data.loginHint,
+        redirectUri: args.data.msalConfig.auth.redirectUri
+    };
+    if (args.data.prompt) request["prompt"] = args.data.prompt;
+    console.debug("acquireTokenRedirect", "request", request);
+
+    try {
+        msalClient.acquireTokenRedirect(request);
+        invokeCallback(args.successCallback, null);
+    }
+    catch (err) {
+        console.error("acquireTokenRedirect", "Failure calling MSAL client", err);
+        invokeCallback(args.failureCallback, err);
+    }
+}
+
+export function getDefaultLoginHint(args) {
+    console.debug("getDefaultLoginHint", args);
+
+    try {
+        let loginHint = getDefaultLoginHintInternal(args);
+        console.debug("getDefaultLoginHint", "loginHint", loginHint);
+        invokeCallback(args.successCallback, loginHint);
+    }
+    catch (err) {
+        invokeCallback(args.failureCallback, err);
+    }
+}
+
+export function handleRedirectPromise(args) {
+    console.debug("handleRedirectPromise", args);
+
+    let msalClient = createMsalClient(args);
+    console.debug("handleRedirectPromise", "msalClient", msalClient);
+
+    try {
+        msalClient.handleRedirectPromise()
+            .then(result => {
+                console.debug("handleRedirectPromise", "success", result);
+                setDefaultLoginHint(args, result);
+                invokeCallback(args.successCallback, result);
+            })
+            .catch(err => {
+                console.error("handleRedirectPromise", "failure", err);
+                invokeCallback(args.failureCallback, err);
+            });
+    }
+    catch (err) {
+        console.error("handleRedirectPromise", "Failure calling MSAL client", err);
+        invokeCallback(args.failureCallback, err);
+    }
+}
+
+export function logout(args) {
+    console.debug("logout", args);
+
+    clearDefaultLoginHint(args);
+
+    let msalClient = createMsalClient(args);
+    let request = {};
+    let logoutUrl = getLogoutUrl(args);
+    if (logoutUrl) {
+        request["postLogoutRedirectUri"] = logoutUrl;
+    }
+
+    console.debug("logout", "request", request);
+
+    try {
+        msalClient.logout(request);
+        invokeCallback(args.successCallback);
+    }
+    catch (err) {
+        invokeCallback(args.failureCallback, err);
+    }
+}
+
+
+
+function clearDefaultLoginHint(args) {
+    console.debug("clearDefaultLoginHint", args);
+    let key = createDefaultLoginHintKey(args);
+    window.localStorage.removeItem(key);
+}
+
+function createDefaultLoginHintKey(args) {
+    return `${args.data.msalConfig.auth.clientId}.blazorade-default-loginHint`;
+}
+
+function createMsalClient(args) {
+    console.debug("createMsalClient", args);
+    setMsalConfigDefault(args.data.msalConfig);
+
+    let msalClient = new msal.PublicClientApplication(args.data.msalConfig);
+    console.debug("createMsalClient", "msalClient", msalClient);
+
+    return msalClient;
+}
+
+function getDefaultAccount(args, msalClient) {
+    console.debug("getDefaultAccount", args, msalClient);
+
+    let account;
+    let loginHint = getDefaultLoginHintInternal(args);
+    console.debug("getDefaultAccount", "Default login hint", loginHint);
+
+    if (loginHint) {
+        account = msalClient.getAccountByUsername(loginHint);
+        console.debug("getDefaultAccount", "Using default account", account);
+    }
+
+    return account;
+}
+
+function getDefaultLoginHintInternal(args) {
+    console.debug("getDefaultLoginHintInternal", args);
+
+    let key = createDefaultLoginHintKey(args);
+    console.debug("getDefaultLoginHintInternal", "key", key);
+
+    let loginHint = window.localStorage.getItem(key);
+    console.debug("getDefaultLoginHintInternal", "loginHint", loginHint);
+
+    return loginHint;
+}
+
+function getLogoutUrl(args) {
+
+    if (args && args.msalConfig && args.msalConfig.auth) {
+        return args.msalConfig.auth["postLogoutRedirectUri"];
+    }
+    return null;
+}
+
+function invokeCallback(callback, ...args) {
+    console.debug("invokeCallback", callback, args);
+    if (callback && callback.target && callback.methodName) {
+        callback.target.invokeMethodAsync(callback.methodName, ...args);
+    }
+    else {
+        console.error("invokeCallbck", "Given method reference cannot be used for invoking a callback.", callback, args);
+    }
+}
+
+function setDefaultLoginHint(args, authResult) {
+    console.debug("setDefaultLoginHint", args, authResult);
+
+    if (authResult && authResult.account && authResult.account.username) {
+        console.debug("setDefaultLoginHint", authResult.account.username);
+
+        let key = createDefaultLoginHintKey(args);
+        window.localStorage.setItem(key, authResult.account.username);
+        console.debug("key", key);
+    }
+}
+
+function setMsalConfigDefault(msalConfig) {
+    console.debug("setMsalConfigDefault", msalConfig);
+
+    msalConfig.auth = msalConfig.auth ? msalConfig.auth : {};
+    msalConfig.auth.redirectUri = msalConfig.auth.redirectUri ? msalConfig.auth.redirectUri : window.location.origin;
+}
